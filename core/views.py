9627c0dfb3dcf190c0e6c2796d65b351aa8bcddf
Carlos <carlossilva@ibict.br>
2024-11-21 11:18:57 -0300

    estrutura

--- core/views.py
+++ core/views.py
@@ -22,6 +22,7 @@ def index(request):
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
     form = TaskForm(instance=task)
+    task_list = Task.objects.all()
 
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
@@ -30,18 +31,19 @@ def updateTask(request, pk):
             form.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = {'task_list': task_list, 'form': form }
 
     return render(request, 'update-task.html', context)
 
 def deleteTask(request, pk):
     task = Task.objects.get(id=pk)
+    task_list = Task.objects.all()
 
     if request.method == 'POST':
         task.delete()
         return redirect('/')
 
-    context = {'task': task}
+    context = {'task_list': task_list,'task': task}
 
     return render(request, 'delete-task.html', context)
 
